14de71e6ed7973750425d6b6fd9866d3190379a5
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-13 09:34:35 -0300

    Add new scientific conversion: 'Number' to 'Categorical'

--- ext/SciTypesUnitfulExt.jl
+++ ext/SciTypesUnitfulExt.jl
@@ -1,9 +1,11 @@
 module SciTypesUnitfulExt
 
 using SciTypes
-using Unitful: AbstractQuantity
+using Unitful: AbstractQuantity, Quantity
 
 SciTypes.scitype(::Type{<:AbstractQuantity{T}}) where {T} = scitype(T)
 SciTypes.sciconvert(::Type{SciTypes.Continuous}, x::AbstractQuantity{<:Integer}) = float(x)
+SciTypes.sciconvert(::Type{SciTypes.Categorical}, x::AbstractQuantity{<:Number}) = convert(Quantity{Int}, x)
+SciTypes.sciconvert(::Type{SciTypes.Categorical}, x::AbstractQuantity{<:Integer}) = x
 
 end
